70aa67133f3d4785088463323884b4e3ca0f02ee
Faizan Munir <faizanmunir017@gmail.com>
2024-10-14 15:09:27 +0500

    My react practice project

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './App.css';
+import {shoot} from './List.ts';
+
+// Props definition
+// interface GreetingProps {
+//   name: string;
+// }
+
+// Functional component using props
+// function Greeting ({ name}:GreetingProps) {
+//   const [message, setMessage] = useState<string>('Hello');
+
+//   return (
+//     <div>
+//       <p>{message}, {name}!</p> {/* Using both state and props */}
+//       <button onClick={() => setMessage('Hey')}>Change Message</button>
+//     </div>
+//   );
+// }
+
+
+// Checking how many times a button has been clicked
+
+
+
+function App() {
+
+  const handleInput=(event:any)=>
+
+  {
+    setValue(event.target.value);    
+  }
+
+  const [count, setCount]= useState(0);
+  const [value,setValue]=useState(" ");
+  const [showText, setShowText] = useState(false); // State to control text visibility
+
+  function clickCount(){
+
+    setCount(count+1);
+  }
+
+  function handleEnter()
+  {
+    setShowText(true);
+  }
+
+  return (
+    <div>
+      <p>Button clicked {count} times</p>
+     <button onClick={clickCount}>Increase</button>
+     <button onClick={()=>shoot("Faizan Scored a goal")}>Shoot</button>
+     <input type="text" onChange={(event)=>handleInput(event)} value={value}></input>
+     <button onClick={(handleEnter)}>Enter</button>
+     {showText && <p>{value}</p>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/List.ts
@@ -0,0 +1,5 @@
+// Testing props , Event handleers etc
+
+export function shoot(name: String) {
+   return alert(name);
+}
